0ac4eaae754b0f9d4b8e439382e04456aa5403d2
Suraj Gawas <suraj.gawas@nike.com>
2020-03-24 17:03:52 -0700

    console

--- src/components/faceRecognizer/faceRecognizer.js
+++ src/components/faceRecognizer/faceRecognizer.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './faceRecognizer.css';
 
 const FaceRecognizer = ({ imageUrl, box }) => {
+    console.log("FaceRecognizer.js=", box);
+    console.log("type of box=", typeof box);
     return (
         <div className='center ma'>
             <div className='absolute mt2'>
                 <img id='inputImage' src={imageUrl} alt='' width='500px' height='auto'/>
-                <div className='bounding-box' 
-                style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
-
-                </div>
+                box.forEach(element => {
+                    <div className='bounding-box' 
+                    style={{top: element.topRow, right: element.rightCol, bottom: element.bottomRow, left: element.leftCol}}>
+    
+                    </div>
+                });
             </div> 
         </div>
     );
